aa498eb4c8e78e9967d2e389eb94b3f584dc1bb1
constantino <44826802+apologeticaly@users.noreply.github.com>
2021-03-03 07:22:13 -0800

    final

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const pauseButton = document.getElementById('button-pause')
 
 playButton.addEventListener('click', (e) => {
   startAudio()
+  console.log(frequencyArray)
   console.log('yo')
 })
 
@@ -33,6 +34,7 @@ function startAudio() {
     frequencyArray = new Uint8Array(analyser.frequencyBinCount)
     // --------------------------------------------------------
 
+    
     audio.play()
 
     pauseButton.addEventListener('click', (e) => {
@@ -43,8 +45,15 @@ function startAudio() {
     requestAnimationFrame(render)
 }
 
+function getColor(x) {
+    if (x%2 == 15) {
+        return 'blue'
+    }
 
-
+    else {
+        return 'white'
+    }
+}
 
 function render() {
 
@@ -55,37 +64,106 @@ function render() {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
 
+    var grd = ctx.createLinearGradient(0, 0, 1500, 500);
+    grd.addColorStop(0, "#c3e29e");
+    grd.addColorStop(0.666, "#e0e29e");
+    grd.addColorStop(1, "#e29e9e");
+
     const centerX = canvas.width / 2
     const centerY = canvas.height / 2
     const radius = canvas.width / 5
-    // -----------------------------------------------
-    ctx.clearRect(0, 0, 300, 300)
-    ctx.beginPath()
-    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
-    ctx.strokeStyle = 'red'
-    ctx.stroke()
-    // ----------------------------------------------
 
-    
-    const bars = 200
-    const step = Math.PI * 2 / bars
+    // -----------------------------------------------
+    ctx.clearRect(0, 0, 1500, 500)
+    ctx.beginPath()
+    // ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+    ctx.strokeStyle = 'white'
+    ctx.stroke()
+    ctx.rect(0,0,1500, 500)
+    ctx.fillStyle=grd;
+    ctx.fill()
+
+    // ----------------------------------------------
+    const bars = 600
+    const step = canvas.width / bars
 
     analyser.getByteFrequencyData(frequencyArray)
 
-    // --------------------------------------------
     frequencyArray.forEach((f, i) => {
-        const barLength = frequencyArray[i] * 0.5
-        const x1 = (Math.cos(step * i) * radius) + centerX
-        const y1 = (Math.sin(step * i) * radius) + centerY
-        const x2 = (Math.cos(step * i) * (radius + barLength)) + centerX
-        const y2 = (Math.sin(step * i) * (radius + barLength)) + centerY
-
+        const barLength = frequencyArray[i]
+        const x1 = (step * i + 2)
+        const y1 = 600
+        const x2 = (step * i + 2)
+        const y2 = (canvas.height) - barLength
+    
+        ctx.beginPath()
         ctx.moveTo(x1, y1)
         ctx.lineTo(x2, y2)
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = getColor(i)
+        ctx.stroke()
+        // ctx.stroke();
     })
+
+
+    // --------------------------------------------
+    // frequencyArray.forEach((f, i) => {
+    //     const barLength = frequencyArray[i] * 0.5
+    //     const x1 = (Math.cos(step * i) * radius) + centerX
+    //     const y1 = (Math.sin(step * i) * radius) + centerY
+    //     const x2 = (Math.cos(step * i) * (radius + barLength)) + centerX
+    //     const y2 = (Math.sin(step * i) * (radius + barLength)) + centerY
+
+    //     ctx.moveTo(x1, y1)
+    //     ctx.lineTo(x2, y2)
+    // })
 
     ctx.stroke()
     // -------------------------------------------------
 
     requestAnimationFrame(render)
 }
+
+// function render() {
+
+//     if (is_playing === false) {
+//         return
+//     }
+    
+//     const canvas = document.getElementById('canvas')
+//     const ctx = canvas.getContext('2d')
+
+//     const centerX = canvas.width / 2
+//     const centerY = canvas.height / 2
+//     const radius = canvas.width / 5
+//     // -----------------------------------------------
+//     ctx.clearRect(0, 0, 300, 300)
+//     ctx.beginPath()
+//     ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI)
+//     ctx.strokeStyle = 'red'
+//     ctx.stroke()
+//     // ----------------------------------------------
+
+    
+//     const bars = 200
+//     const step = Math.PI * 2 / bars
+
+//     analyser.getByteFrequencyData(frequencyArray)
+
+//     // --------------------------------------------
+//     frequencyArray.forEach((f, i) => {
+//         const barLength = frequencyArray[i] * 0.5
+//         const x1 = (Math.cos(step * i) * radius) + centerX
+//         const y1 = (Math.sin(step * i) * radius) + centerY
+//         const x2 = (Math.cos(step * i) * (radius + barLength)) + centerX
+//         const y2 = (Math.sin(step * i) * (radius + barLength)) + centerY
+
+//         ctx.moveTo(x1, y1)
+//         ctx.lineTo(x2, y2)
+//     })
+
+//     ctx.stroke()
+//     // -------------------------------------------------
+
+//     requestAnimationFrame(render)
+// }
